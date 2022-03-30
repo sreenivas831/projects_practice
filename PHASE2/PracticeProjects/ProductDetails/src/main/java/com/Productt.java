3e6ad8ffc7422d2e0b7ebb88d879ae47c49d3982
sreenivas <sree831762@gmail.com>
2022-03-30 20:15:42 +0530

    changesdone

---
+++ PHASE2/PracticeProjects/ProductDetails/src/main/java/com/Productt.java
@@ -0,0 +1,41 @@
+package com;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Productt
+ */
+public class Productt extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Productt() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String pId = request.getParameter("productId");
+		String pName = request.getParameter("productName");
+		String pPrice = request.getParameter("productPrice");
+		
+		HttpSession theSession = request.getSession();
+		
+		theSession.setAttribute("pid", pId);
+		theSession.setAttribute("pname", pName);
+		theSession.setAttribute("pprice", pPrice);
+		
+		response.sendRedirect("display.jsp");
+	}
+
+}
